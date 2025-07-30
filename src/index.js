74b057cf9ceed960794250458a9fb4facd471dfd
aashikantkumar <aashikant.kumar2023@iem.edu.in>
2025-07-31 00:30:46 +0530

    feat: Initialize project with Express and MongoDB setup
    - Created package.json with necessary dependencies and scripts.
    - Implemented Express app with CORS and cookie-parser middleware.
    - Added constants for database configuration.
    - Developed user controllers for registration, login, logout, and token generation.
    - Established MongoDB connection utility.
    - Set up multer middleware for file uploads.
    - Defined Mongoose models for users and chefs with password hashing and token generation methods.
    - Created user routes for registration with file upload handling.
    - Implemented utility classes for API error handling and responses.
    - Added async handler utility for managing asynchronous route handlers.
    - Integrated Cloudinary for image uploads with error handling.

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+import {app} from "./app.js";
+import mongoose from "mongoose";
+
+dotenv.config({
+
+    path:'./env'
+})
+
+connectDB()
+
+ .then(
+    ()=>{
+        app.listen(process.env.PORT|| 8000,()=>{
+
+            console.log(`Server is running at port:${process.env.PORT}`)
+        })
+
+        app.on("error",(error)=>{
+            console.log("ERRR:",error);
+            throw error
+        })
+    }) 
+   .catch((err)=>{
+    console.log("Mogo db connection failed !!!",err)
+       
+    }  
+    )
